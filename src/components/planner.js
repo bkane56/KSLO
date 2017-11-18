7da6621bc793e64500833654f879d1b57439a7db
Brian Kane <bkane56@gmail.com>
2017-11-18 17:18:27 -0600

    still trying

--- src/components/planner.js
+++ src/components/planner.js
@@ -1,61 +1,70 @@
 import React, { Component } from 'react';
+import { Glyphicon, Col, Row } from 'react-bootstrap';
+import BigCalendarCSS from 'react-big-calendar/lib/css/react-big-calendar.css';
+import '../style/App.css';
 import BigCalendar from 'react-big-calendar';
 import Moment from 'moment';
-import '../style/App.css';
-import '../style/calendar.css';
-import { Events } from '../resources/events';
-import '../../node_modules/react-big-calendar/lib/css/react-big-calendar.css';
+import { Events } from '../utils/events'
 
 
-BigCalendar.setLocalizer(BigCalendar.momentLocalizer(Moment));
+BigCalendar.setLocalizer(
+    BigCalendar.momentLocalizer(Moment)
+);
 
 export default class Planner extends Component {
-  constructor(props, context) {
-    super(props, context);
-    this.context = context;
-    this.handleSelectSlot = this.handleSelectSlot.bind(this);
-    this.handleSelectEvent = this.handleSelectEvent.bind(this);
-    this.state = { events: Events };
-  }
+    constructor(props, context)
+    {
+        super(props, context);
+        this.context = context;
+        console.log("Events = " + Events)
+        this.state = {events: Events};
+    }
 
-  handleSelectSlot({ start, end }) {
-    // create an event with title "Test"
-    this.state.events.push({ start, end, title: 'Test' });
-    this.setState({});
-  }
+    handleSelectSlot({start, end})
+    {
+        //create an event with title "Test"
+        console.log("handleSelectSlot: " + start + " - " + end);
+        this.state.events.push({start: start, end: end, title: "Test"});
+        this.setState({});
 
-  handleSelectEvent() {
-    // just for example
-    console.log(`handleSelectEvent: ${JSON.stringify(arguments)}`);
-  }
+    }
 
-  EventWeek(props) {
-    return <strong>{props.event.title}</strong>;
-  }
+    handleSelectEvent()
+    {
+        //just for example
+        console.log("handleSelectEvent: " + JSON.stringify(arguments));
+    }
 
-  EventAgenda(props) {
-    return <em>{props.event.title}</em>;
-  }
+    EventWeek(props)
+    {
+        return <strong>{props.event.title}</strong>
+    }
 
-  render() {
-    return (
-      <div className="Calendar">
-        <BigCalendar
-          selectable
-          popup
-          events={this.state.events}
-          onSelectSlot={this.handleSelectSlot}
-          onSelectEvent={this.handleSelectEvent}
-          views={['month', 'week', 'day']}
-          defaultDate={new Date(2017, 11, 1)}
-          eventPropGetter={e => ({ className: 'test-class' })} /* Here you can define a style for the element */
-          components={{
+    EventAgenda(props)
+    {
+        return <em>{props.event.title}</em>
+    }
+
+    render()
+    {
+        return (
+            <div className='Calendar'>
+                <BigCalendar
+                    selectable
+                    popup
+                    events={this.state.events}
+                    onSelectSlot={this.handleSelectSlot.bind(this)}
+                    onSelectEvent={this.handleSelectEvent.bind(this)}
+                    views={['month', 'week', 'day']}
+                    defaultDate={new Date(2017, 11, 1)}
+                    eventPropGetter={e => ({ className: 'test-class'})} /* Here you can define a style for the element */
+                    components={{
                         event: this.EventWeek,
                         agenda: {
-                            event: this.EventAgenda,
-                        },
+                            event: this.EventAgenda
+                        }
                     }}
-        />
-      </div>);
-  }
+                />
+        </div>);
+    }
 }
